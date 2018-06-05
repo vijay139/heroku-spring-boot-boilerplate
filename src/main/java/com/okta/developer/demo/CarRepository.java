66884cb68590de989201db4127b1ba73ca90605b
Krishna Prasad <skprasadu@yahoo.com>
2018-06-04 17:05:15 -0700

    first commit

---
+++ src/main/java/com/okta/developer/demo/CarRepository.java
@@ -0,0 +1,10 @@
+package com.okta.developer.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource
+@CrossOrigin(origins = "https://still-ravine-79015.herokuapp.com")
+interface CarRepository extends JpaRepository<Car, Long> {
+}
